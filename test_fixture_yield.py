e05bb71974900ad254e5e57194acb42251edbd2a
tangfeng-sys <913164916@qq.com>
2020-03-25 09:53:46 +0800

    py文件

---
+++ test_fixture_yield.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#作用域;module是在模块之前执行，模块之后执行
+import pytest
+@pytest.fixture(scope="module")
+def open():
+    print("打开浏览器")
+    yield
+    print("执行teardown！")
+    print("最后关闭浏览器")
+
+def test_search1(open):
+    print("test_search1")
+    #raise NameError
+    pass
+def test_search2(open):
+    print("test_search2")
+    #raise NameError
+    pass
+def test_search3(open):
+    print("test_search3")
+    #raise NameError
+    pass
+if __name__ == "__main__":
+    pytest.main()
